46a7c1bc903d6e33da6d9990d5387339d03ba383
Brandon Skar <bransk01@hotmail.com>
2025-12-10 16:20:48 -0800

    Wall jump and FLW

--- src/data/shortcuts/RRy.js
+++ src/data/shortcuts/RRy.js
@@ -32,12 +32,74 @@ export const RRy_shortcuts = {
 			description: "At the end of the big ramp, jump off to the left to skip some of the track",
 			steps: [
 				{
-					title: "At the end of the big ramp slide off the ramp to the left at the right angle as close to the railing as possible",
+					title: "At the end of the big ramp slide off the ramp to the left at the correct angle as close to the railing as possible",
 					snipClips: [
 						`${process.env.PUBLIC_URL}/assets/videos/RRy/ramp-jump.mp4`,
 					]
 				}
 			],
+		},
+		{
+			id: "flw-ramp-jump",
+			title: "Finish Line Warp (FLW)",
+			description: "Doing the ramp just a certain way will trick Lakitu into putting you down on the finish line and immediately trigger the lap\n" +
+			"NOTE: This really only saves time on the last lap since Lakitu puts you down facing backwards and you'll have to spend time turning around to get going again",
+			steps: [
+				{
+					title: "At the end of the big ramp slide off the ramp to the left at the correct angle a little bit away from the railing" +
+					"You want to be as far away from the road as possible when you land but still trigger the warp from the Ramp Jump shortcut",
+					images: [
+						`${process.env.PUBLIC_URL}/assets/images/RRy/flw-ramp-jump.png`,
+					],
+				}
+			],
+		},
+		{
+			id: "wall-jump",
+			title: "Wall Jump",
+			description: "Just after the first turn you can jump on to of the red/white wall and have Lakitu put you down on the big ramp" +
+			"NOTE: YOU HAVE TO DO River Jump for it to work but doing the Ramp Jump/FLW shortcut breaks this shortcut",
+			steps: [
+				{
+					title: "Do the River Jump to get the shortcut to start working",
+				},
+				{
+					title: "After the finish line at the first turn don't go into the grass too early, wait until you are at the bridge shadow at least",
+					images: [
+						`${process.env.PUBLIC_URL}/assets/images/RRy/bridge-shadow.png`,
+					],
+				},
+				{
+					title: "Go a little bit past the first turn and turn right, align yourself with the image\n" +
+					"TIP: If you're using the ghost, It's hard to see but you shoould see a line going up and right, you want to hit near left part of the line\n" +
+					"There should be a small amount of the white square to the right of the item box",
+					images: [
+						`${process.env.PUBLIC_URL}/assets/images/RRy/wall-jump-alignment.png`,
+						`${process.env.PUBLIC_URL}/assets/images/RRy/wall-jump-alignment-markings.png`,
+					],
+				},
+				{
+					title: "Shroom and press R about half way across the road"
+				},
+				{
+					title: "When you land it should bounce you back up in the air. Then right as you're landing again Press R" +
+					"NOTE: You only need to press R once on the road and again when you're about to hit the wall\n" +
+					"But it's ok to just spam R as well",
+					snipClips: [
+						`${process.env.PUBLIC_URL}/assets/videos/RRy/wall-jump.mp4`,
+					]
+				}
+			],
+			resources: [
+				{
+					label: "Wall Jump Setup Ghost",
+					link: "https://discord.com/channels/692171166034427916/1117106342784671784/1270116173610225695"
+				},
+				{
+					label: "Converter for EverDrive / DexDrive",
+					link: "https://beckabney.com/mk64/mempak.php"
+				}
+			]
 		},
 	],
 };
